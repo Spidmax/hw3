95e9e11ec2de9a8c5d89bb061508ee2719274067
Spidmax <spidmax@gmail.com>
2017-09-24 13:34:09 -0700

    Create exercise5.js

---
+++ exercise5.js
@@ -0,0 +1,24 @@
+strokeWeight(3);
+stroke(0);
+noFill();
+
+x = 0;
+
+while (true) {
+  
+  x = x + 10;
+  
+	while (x < 200) {
+  	background(255);
+  	ellipse(100+x, 100, 50, 50)
+		x = x + 10;
+	}
+  
+  while (x > -80) {
+	background(255);
+  ellipse(100+x, 100, 50, 50)
+	x = x - 10;
+	}
+  
+}
+

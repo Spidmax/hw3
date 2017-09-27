5819deb3b43b2359ed034a710484bd5f9a5b0982
Spidmax <spidmax@gmail.com>
2017-09-27 08:17:06 -0700

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -2,16 +2,16 @@ strokeWeight(3);
 stroke(0);
 noFill();
 
-x = 0;
+var x = 0;
 
 while (x < 200) {
-  background(255);
-  ellipse(100+x, 100, 50, 50)
+	background(255);
+	ellipse(100+x, 100, 50, 50)
 	x = x + 10;
 }
 
 while (x > -80) {
 	background(255);
-  ellipse(100+x, 100, 50, 50)
+	ellipse(100+x, 100, 50, 50)
 	x = x - 10;
 }
